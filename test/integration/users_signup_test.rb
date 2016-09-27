20e9717074d888794c42ab4a6ef988b9c15eb609
Takuto Sugita <s.takuto0214@gmail.com>
2016-09-27 10:38:43 +0900

    Finish user signup

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -4,4 +4,26 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+
+  test "invalid signup information" do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post users_path, user: {name: "",
+                              email: "user@invalid",
+                              password:              "foo",
+                              password_confirmation: "bar" }
+    end
+    assert_template 'users/new'
+  end
+
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post_via_redirect users_path, user: { name: "takuto",
+                                          email: "s.takuto0314@gmail.com",
+                                          password: "123456",
+                                          password_confirmation: "123456" }
+    end
+    assert_template 'users/show'
+  end
 end
