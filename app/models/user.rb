7fefb02ef4605b09e4fedb44c22dc78ddcd1493b
Takuto <s.takuto0214@gmail.com>
2016-09-28 23:00:31 +0900

    do until 8.4

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,10 @@ class User < ActiveRecord::Base
   validates :email, presence: true, length: { maximum: 255 },
     format: { with: VALID_EMAIL_REGEX},
     uniqueness: { case_sensitive: false }
+
+
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
 end
